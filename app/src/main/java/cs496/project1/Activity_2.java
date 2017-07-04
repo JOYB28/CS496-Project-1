27e9957cb7796c3841049027234bca944bcaeeee
JOYN28 <kimyoonseo@kaist.ac.kr>
2017-07-05 01:09:38 +0900

    final first project-2

--- app/src/main/java/cs496/project1/Activity_2.java
+++ app/src/main/java/cs496/project1/Activity_2.java
@@ -65,20 +65,20 @@ public class Activity_2 extends AppCompatActivity {
 
         TabHost.TabSpec ts1 = tabHost1.newTabSpec("Tab Spec 1");
         ts1.setContent(R.id.content1);
-        ts1.setIndicator("TAB1");
+        ts1.setIndicator("Contacts");
         tabHost1.addTab(ts1);
 
 
         TabHost.TabSpec ts2 = tabHost1.newTabSpec("Tab Spec 2");
         ts2.setContent(R.id.content2);
-        ts2.setIndicator("TAB2");
+        ts2.setIndicator("Gallary");
 
         tabHost1.addTab(ts2);
 
 
         TabHost.TabSpec ts3 = tabHost1.newTabSpec("Tab Spec 3");
         ts3.setContent(R.id.content3);
-        ts3.setIndicator("TAB3");
+        ts3.setIndicator("Quiz");
         tabHost1.addTab(ts3);
 
         //버튼
